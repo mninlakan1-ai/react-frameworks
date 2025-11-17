ddfab986b735ebf683b9d46e0d2bead8c6f51220
mninlakan1-ai <m.ninlakan1@gmail.com>
2025-11-17 11:44:50 +0700

    first commit

---
+++ src/component/Footer.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <footer className="bg-gray-700 text-white shadow-inner pt-10 pb-6">
+        <div className="max-w-6xl mx-auto px-6 flex flex-col md:flex-row justify-between gap-8">
+          <div className="flex-1">
+            <h1 className="font-bold text-lg mb-2">My Website</h1>
+            <p className="text-gray-200 leading-relaxed">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore,
+              error pariatur in accusantium consequatur natus esse consequuntur
+              adipisci consectetur doloribus!
+            </p>
+          </div>
+
+          <div className="flex-1">
+            <h1 className="font-bold text-lg mb-2">About</h1>
+            <p className="text-gray-200 leading-relaxed">
+              Name: Ninlakan Labanluang<br />
+              Nickname: Mint <br />
+            </p>
+          </div>
+
+          <div className="flex-1">
+            <h1 className="font-bold text-lg mb-2">Contact</h1>
+            <p className="text-gray-200 leading-relaxed">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda laboriosam voluptatum veritatis voluptas, aliquam aut eos ducimus optio itaque explicabo?
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
